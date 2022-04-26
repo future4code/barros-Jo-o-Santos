52b41f2d360d5f33723ff8389e91c6e42190c465
joaopdev <you@example.com>
2022-04-26 20:53:26 -0300

    Adicionando arquivos

---
+++ modulo1/objetos/index.js
@@ -0,0 +1,32 @@
+// Exercício  1
+// 1) Matheus Nachtergaele  / 2) Virginia Cavendish
+// 3) canal: 'Globo', horario: '14h'
+
+
+//Exercicio 2 
+//A)
+// 1 ) Nome: Juca idade: 3 raca: "srd" 
+//2) Nome: Juba idade:3 raca : "srd"
+//3) nome: Jubo idade 3 raca srd 
+ 
+// b) Spread :  significa espalhar é usado para espalhar os elementos de um array!
+
+// Exercicio 3 
+
+//A) False /  undefined 
+//B) porque o valor atibuido ao backender era false, e altura não tinha definino na const! 
+
+function tresApelidos( nome, apelidos) {
+    return nome[apelidos]
+
+}
+
+const pessoa ={
+    nome: "joão Pedro"
+    Apelidos:"joão" , "mineiro" , "Pedro"
+}
+
+console.log(tresApelidos(pessoa.Apelidos))
+
+
+
